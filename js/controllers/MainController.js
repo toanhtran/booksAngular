61c87c93ee2e8ba2f204a796c0e9ec3fbb5d7e5f
toanhtran <toanh_tran@yahoo.com>
2016-01-10 23:54:44 +0900

    update book list

--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -17,22 +17,6 @@ app.controller('MainController', ['$scope', function($scope) {
     cover: 'img/program-or-be-programmed.jpg',
     likes: 0,
     dislikes: 0
-  } ,
-    { 
-    name: 'Broadcasting Happiness', 
-    price: 24.99, 
-    pubdate: new Date('2014', '03', '08'), 
-    cover: 'img/the-book-of-trees.jpg',
-    likes: 0,
-    dislikes: 0
-  }, 
-  { 
-    name: 'The Art of Driving in the Rain', 
-    price: 10, 
-    pubdate: new Date('2013', '08', '01'), 
-    cover: 'img/program-or-be-programmed.jpg' ,
-    likes: 0,
-    dislikes: 0
   } 
 ]
   $scope.plusOne = function(index) { 
